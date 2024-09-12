738bdbf6268afaa438a63ab767d75aee5401374d
EladioJES <elajimsar@gmail.com>
2024-09-12 13:43:50 +0200

    second commit

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://EladioJES.github.io',
                 'git_url': 'https://github.com/EladioJES/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('notcore.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.notcore': {'nbdev_hello_world.notcore.foo': ('notcore.html#foo', 'nbdev_hello_world/notcore.py')}}}
